ee7b1d4d9bcab0b50fc9797ca372601d237efdfa
Ket✦Ralus <ket@ket-ralus.com>
2020-04-25 04:46:44 -0700

    make post_titles display more consistent, stylable

--- ext/post_titles/theme.php
+++ ext/post_titles/theme.php
@@ -4,17 +4,15 @@ class PostTitlesTheme extends Themelet
     public function get_title_set_html(string $title, bool $can_set): string
     {
         $html = "
-			<tr>
+			<tr class='post_title'>
 				<th>Title</th>
 				<td>
-		".($can_set ? "
 					<span class='view'>".html_escape($title)."</span>
+		".($can_set ? "
 					<span class='edit'>
-						<input class='edit'  type='text' name='post_title' value='".html_escape($title)."' />
+						<input class='edit' type='text' name='post_title' value='".html_escape($title)."' />
 					</span>
-		" : html_escape("
-					 $title
-		"))."
+		" : "")."
 				</td>
 			</tr>
 		";
