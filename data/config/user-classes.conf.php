ce2c4c5e6a9374ba0657e0c4c3ee72c47f7abf0c
Ket✦Ralus <ket@ket-ralus.com>
2020-12-04 20:01:12 -0800

    restrict users from editing image rating, tags, or other data

--- data/config/user-classes.conf.php
+++ data/config/user-classes.conf.php
@@ -1,5 +1,14 @@
 <?php
 new UserClass("user", "user", [
 	Permissions::CREATE_IMAGE => False,
+
+	Permissions::EDIT_IMAGE_RATING => False,
+	Permissions::BULK_EDIT_IMAGE_RATING => False,
+
+	Permissions::EDIT_IMAGE_TAG => False,
+	Permissions::BULK_EDIT_IMAGE_TAG => False,
+
+	Permissions::MASS_TAG_EDIT => False,
+	Permissions::EDIT_TAG_CATEGORIES => False,
 ]);
 ?>
