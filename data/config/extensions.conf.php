fca2f76a75eb44cfe8840c93a126ebb73d0d32a6
Ket✦Ralus <ket@ket-ralus.com>
2022-07-14 03:12:23 -0500

    Enable image hash ban extension

--- data/config/extensions.conf.php
+++ data/config/extensions.conf.php
@@ -1,3 +1,3 @@
 <?php
-define("EXTRA_EXTS", "admin,autocomplete,blocks,bulk_add,custom_html_headers,favorites,handle_video,home,ket_metadata,pools,post_titles,random_list,rating,regen_thumb,relationships,resize,rss_comments,rss_images,wiki");
+define("EXTRA_EXTS", "admin,autocomplete,blocks,bulk_add,custom_html_headers,favorites,handle_video,home,image_hash_ban,ket_metadata,pools,post_titles,random_list,rating,regen_thumb,relationships,resize,rss_comments,rss_images,wiki");
 ?>
