47bc1ff14b25323acd68a02a03b9100e6a597791
Ket✦Ralus <ket@ket-ralus.com>
2022-07-16 04:48:53 -0500

    Add directory indexing and error document

---
+++ .dir_index_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+		<title>ket-ralus.com<?php echo $_SERVER['REQUEST_URI']; ?></title>
+		<link rel="SHORTCUT ICON" href="https://ket-ralus.com/favicon.ico" />
+		<script type="text/javascript">//:fufouc:</script>
+		<style type="text/css" media="screen">
+			@import "https://ket-ralus.com/.dir_index_style.css";
+		</style>
+	</head>
+	<body>
+		<div id="divBase">
+			<h1>ket-ralus.com</h1>
+			
+			<h2><?php echo $_SERVER['REQUEST_URI']; ?></h2>
---
+++ .error.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel='stylesheet' href='/themes/ket-ralus/style.css' type='text/css' />
+	</head>
+	<body>
+		<header>
+			<h1 style='padding: 1rem 0 0 1rem;'>Ket-Ralus.com : 404 (File Not Found)</h1>
+		</header>
+		<article style='margin: 0'>
+			<section>
+				<p style='padding-left: 1rem;'>The file you are trying to access has likely been moved. Go to <a href='https://gallery2.ket-ralus.com/'>my gallery</a> and you may find what you are looking for.</p>
+			</section>
+		</article>
+	</body>
+</html>
